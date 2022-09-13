e1d07dcea7915e2df2ddb1727c9a8f61ab08e5b1
Vinicius Franco <viniciusfrancoma09@hotmail.com>
2022-09-13 02:49:48 -0300

    Details screen implementation

--- starter/app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
@@ -2,12 +2,22 @@ package com.udacity.project4.locationreminders
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
 import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityReminderDescriptionBinding
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
+import java.util.*
 
 /**
  * Activity that displays the reminder details after the user clicks on the notification
@@ -15,6 +25,7 @@ import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 class ReminderDescriptionActivity : AppCompatActivity() {
 
     companion object {
+        private const val TAG = "ReminderDescription"
         private const val EXTRA_ReminderDataItem = "EXTRA_ReminderDataItem"
 
         //        receive the reminder object after the user clicks on the notification
@@ -25,13 +36,66 @@ class ReminderDescriptionActivity : AppCompatActivity() {
         }
     }
 
+    private lateinit var map : GoogleMap
     private lateinit var binding: ActivityReminderDescriptionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val reminder = intent.extras?.get(EXTRA_ReminderDataItem) as ReminderDataItem
+
         binding = DataBindingUtil.setContentView(
             this,
             R.layout.activity_reminder_description
         )
-//        TODO: Add the implementation of the reminder details
+        binding.reminderDataItem = reminder
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        map = googleMap
+
+        binding.reminderDataItem?.let {
+            val zoomLevel = 15f
+            val reminderLatLong = LatLng(it.latitude ?: 0.0, it.longitude ?: 0.0)
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(reminderLatLong, zoomLevel))
+
+            val snippet = String.format(
+                Locale.getDefault(),
+                getString(R.string.lat_long_snippet),
+                it.latitude,
+                it.longitude
+            )
+
+            map.addMarker(
+                MarkerOptions()
+                    .position(reminderLatLong)
+                    .snippet(snippet)
+                    .title(it.location)
+            ).showInfoWindow()
+        }
+
+        setMapStyle(map)
+    }
+
+    private fun setMapStyle(map: GoogleMap) {
+        try {
+            // Customize the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            val success = map.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    this,
+                    R.raw.map_style
+                )
+            )
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error: ", e)
+        }
     }
 }
